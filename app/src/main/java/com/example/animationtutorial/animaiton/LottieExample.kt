5c78615fb6b48098fbfa699d43656b084613fcbf
gultencelikbilek <81778970+gultencelikbilek@users.noreply.github.com>
2024-08-08 11:07:48 +0300

    added Lottie

---
+++ app/src/main/java/com/example/animationtutorial/animaiton/LottieExample.kt
@@ -0,0 +1,54 @@
+package com.example.animationtutorial.animaiton
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.animationtutorial.R
+
+@Composable
+fun LottieExample() {
+    val compositon by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie))
+    var isPlaying by remember { mutableStateOf(true) }
+    val progress by animateLottieCompositionAsState(
+        composition = compositon,
+        iterations = LottieConstants.IterateForever
+    )
+
+    LaunchedEffect(key1 = progress) {
+        if (progress == 0f) {
+            isPlaying = true
+        }
+        if (progress == 1f) {
+            isPlaying = false
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimation(
+            modifier = Modifier.size(400.dp),
+            composition = compositon,
+            progress = progress
+        )
+    }
+}
